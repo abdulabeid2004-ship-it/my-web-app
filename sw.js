fe861e1fc46513024bd4e5c840439bdeca09fffa
abdulabeid2004-ship-it <abdulabeid2004@gmail.com>
2026-08-09 14:23:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,50 @@
-self.addEventListener('fetch', () => {});
+const CACHE_NAME = 'aka-house-chore-v1';
+const urlsToCache = [
+  '/my-web-app/',
+  '/my-web-app/index.html',
+  '/my-web-app/manifest.json',
+  '/my-web-app/icons/icon-192.png',
+  '/my-web-app/icons/icon-512.png'
+];
+
+// Install the service worker and cache assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update the service worker and clean old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
